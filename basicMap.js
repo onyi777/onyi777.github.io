409b21880dc819f6066ce8ef94ede5c20118f573
onyi777 <onyiokoye657@gmail.com>
2024-07-11 16:37:42 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -2,4 +2,13 @@ console.log("basicMap.js");
 
 // CPCC Coordinates: 35.2176665 N, 80.831473 W -> 35.217665, -80.831473
 
-let map = l.map("map");
+let map = l.map("map",(
+  center:[35.217665, -80.831473],
+  zoom: 16
+
+  ));
+  
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
